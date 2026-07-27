8eebda20ee5e82ae534c9aaa994bdb42b99172a7
Sean McElholm <seanmcelholm@gmail.com>
2026-07-27 15:04:18 +0100

    Converted Vehicles,json to a line-oriented format more appropriate for the GTA3-era.

--- Assets/Scripts/Vehicles/VehicleManager.cs
+++ Assets/Scripts/Vehicles/VehicleManager.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.IO;
 using UnityEngine;
 
@@ -23,14 +24,103 @@ namespace GTA3Unity.Vehicles
     {
         public static VehicleData VehicleData { get; private set; }
 
-        public static bool Init(string pathToVehicleJson)
+        public static bool Init(string pathToVehicleSettingsDat)
         {
-            if(!File.Exists(pathToVehicleJson))
+            if(!File.Exists(pathToVehicleSettingsDat))
             {
                 return false;
             }
 
-            VehicleData = JsonUtility.FromJson<VehicleData>(File.ReadAllText(pathToVehicleJson));
+            VehicleData vehicleData = new();
+
+            string[] lines = File.ReadAllLines(pathToVehicleSettingsDat);
+            for (int lineIndex = 0; lineIndex < lines.Length; lineIndex++)
+            {
+                string line = lines[lineIndex];
+                int commentIndex = line.IndexOf(';');
+                if (commentIndex >= 0)
+                {
+                    line = line.Substring(0, commentIndex);
+                }
+
+                line = line.Trim();
+                if (line.Length == 0)
+                {
+                    continue;
+                }
+
+                string[] settings = line.Split(
+                    new[] { ' ', '\t' },
+                    StringSplitOptions.RemoveEmptyEntries);
+                if (settings.Length != 2)
+                {
+                    return false;
+                }
+
+                string settingName = settings[0];
+                string value = settings[1];
+                bool parsed = settingName switch
+                {
+                    "ReverseGear" => int.TryParse(
+                        value,
+                        NumberStyles.Integer,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.ReverseGear),
+                    "ReverseSpeedRatio" => float.TryParse(
+                        value,
+                        NumberStyles.Float,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.ReverseSpeedRatio),
+                    "ShiftDownFraction" => float.TryParse(
+                        value,
+                        NumberStyles.Float,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.ShiftDownFraction),
+                    "StopSpeed" => float.TryParse(
+                        value,
+                        NumberStyles.Float,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.StopSpeed),
+                    "InputDeadZone" => float.TryParse(
+                        value,
+                        NumberStyles.Float,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.InputDeadZone),
+                    "HandbrakeTorque" => float.TryParse(
+                        value,
+                        NumberStyles.Float,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.HandbrakeTorque),
+                    "LowerGearSpeedMultiplier" => float.TryParse(
+                        value,
+                        NumberStyles.Float,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.LowerGearSpeedMultiplier),
+                    "BrakeForceMultiplier" => float.TryParse(
+                        value,
+                        NumberStyles.Float,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.BrakeForceMultiplier),
+                    "WheelGripMultiplier" => float.TryParse(
+                        value,
+                        NumberStyles.Float,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.WheelGripMultiplier),
+                    "GameSpeedToMetersPerSecond" => float.TryParse(
+                        value,
+                        NumberStyles.Float,
+                        CultureInfo.InvariantCulture,
+                        out vehicleData.GameSpeedToMetersPerSecond),
+                    _ => true
+                };
+
+                if (!parsed)
+                {
+                    return false;
+                }
+            }
+
+            VehicleData = vehicleData;
             return true;
         }
     }
